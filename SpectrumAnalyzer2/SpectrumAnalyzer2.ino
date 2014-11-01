fec23e08f650440623f146871373c2dd0c76c26b
Mike Newell <mike_newell@gspsf.com>
2014-11-01 10:43:47 -0700

    working with spectrum analyzer, this is pretty good but I think I need the moving average to make everything relative

--- SpectrumAnalyzer2/SpectrumAnalyzer2.ino
+++ SpectrumAnalyzer2/SpectrumAnalyzer2.ino
@@ -6,7 +6,7 @@
 #define N_PIXELS  120  // Number of pixels you are using
 #define LED_PIN   13  // NeoPixel LED strand is connected to GPIO #0 / D0
 #define SPECTRUM 0 // the spectrum to follow
-#define HUE_SPEED 0.001; // .001 takes about 20 minutes
+#define HUE_SPEED 0.0001; // .001 takes about 20 minutes
 
 Adafruit_NeoPixel  strip = Adafruit_NeoPixel(N_PIXELS, LED_PIN, NEO_GRB + NEO_KHZ800);
 
@@ -23,16 +23,31 @@ int skip;
 // Spectrum analyzer read values will be kept here.
 int Spectrum[7];
 
+// keep a running avg to cancel out volume adjustment
+const int numReadings = 150;
+int readings[numReadings];      // the readings from the analog input
+int index = 0;                  // the index of the current reading
+int total = 0;                  // the running total
+int average = 0;                // the average
+
 void setup() {
-  byte Counter;
+//  byte Counter;
   
   Serial.begin(9600);
   
+  // leds
   hue = 0.0;
   skip = 7;
-  
   strip.begin();
   strip.show(); // Initialize all pixels to 'off'
+  
+  delay(100);
+  
+  // running avg
+  for (int thisReading = 0; thisReading < numReadings; thisReading++)
+    readings[thisReading] = 0;
+
+  delay(100);
 
   //Setup pins to drive the spectrum analyzer. 
   pinMode(spectrumReset, OUTPUT);
@@ -55,15 +70,48 @@ void setup() {
 
 void loop() {
 
-  int Counter, Counter2, Counter3;
-  
+//  int Counter, Counter2, Counter3;
+
   // slowly adjust the hue overtime to red instead of blue
   hue += HUE_SPEED;
-    
-//  showSpectrum();
+
+  // read the latest
   readSpectrum();
+  
+  // RUNNING AVG
+  // subtract the last reading:
+  total= total - readings[index]; 
+  // read from the spectrum:  
+  readings[index] = Spectrum[SPECTRUM];
+  // add the reading to the total:
+  total= total + readings[index];       
+  // advance to the next position in the array:  
+  index = index + 1;
+  // if we're at the end of the array...
+  if (index >= numReadings)              
+    // ...wrap around to the beginning: 
+    index = 0;
+  // calculate the average:
+  average = total / numReadings;
+  
+  // send it to the computer as ASCII digits
+  Serial.println(average);
+  
+  strip.setBrightness(Spectrum[SPECTRUM]);
+  if(skip <= 0) {
+    int spread = random(0, 10000)/Spectrum[SPECTRUM]; // get an even spread across the pixels
+    uint32_t col = strip.Color(abs(sin(Spectrum[SPECTRUM])*25*hue),abs(cos(Spectrum[SPECTRUM])*25),constrain(Spectrum[SPECTRUM] - hue, 0, 255));
+    strip.setPixelColor(spread,col);  
+    skip = (1000 * average) / (Spectrum[SPECTRUM]*Spectrum[SPECTRUM]);
+  }
+  skip--;
+  strip.show(); // Update strip 
+  
+  
+  
+//  Serial.println(rIdx);
+  
   delay(15);  //We wait here for a little while until all the values to the LEDs are written out.
-              
   
 }
 
@@ -75,21 +123,9 @@ void readSpectrum()
   for(Band=0;Band <7; Band++)
   {
     Spectrum[Band] = (analogRead(spectrumAnalog) + analogRead(spectrumAnalog) ) >>1; //Read twice and take the average by dividing by 2
-    Serial.println(Spectrum[SPECTRUM]);
+    
     digitalWrite(spectrumStrobe,HIGH);
     digitalWrite(spectrumStrobe,LOW);
-
-    strip.setBrightness(Spectrum[SPECTRUM]);
-      
-      if(skip <= 0) {
-        int spread = random(0, 10000)/Spectrum[SPECTRUM]; // get an even spread across the pixels
-        uint32_t col = strip.Color(abs(sin(Spectrum[SPECTRUM])*25*hue),abs(cos(Spectrum[SPECTRUM])*25),constrain(Spectrum[SPECTRUM] - hue, 0, 255));
-        strip.setPixelColor(spread,col);  
-        skip = 10000 / (Spectrum[SPECTRUM]*Spectrum[SPECTRUM]);
-      }
-      skip--;
-      
-    strip.show(); // Update strip 
   }
 }
 
